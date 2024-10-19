c368ac902d4587704abfacd996b90dfb6dd8b519
florezandres <andres.f1850@gmail.com>
2024-10-19 11:10:44 -0500

    HU-43 - Eliminar un tramite que este incompleto v2

--- Colombina-dev/colombina/src/main/java/com/example/colombina/controllers/TramiteController.java
+++ Colombina-dev/colombina/src/main/java/com/example/colombina/controllers/TramiteController.java
@@ -1,15 +1,9 @@
 package com.example.colombina.controllers;
 
+import com.example.colombina.services.TramiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.colombina.services.TramiteService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tramites")
