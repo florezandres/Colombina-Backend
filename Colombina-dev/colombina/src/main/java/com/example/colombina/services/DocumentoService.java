557aaf0f84289304e3f7577ec86258f0337630d9
Estebana Altamiranda <jaltamiranda187@gmail.com>
2024-10-20 01:08:32 +0000

    Historia de ususario 51

--- Colombina-dev/colombina/src/main/java/com/example/colombina/services/DocumentoService.java
+++ Colombina-dev/colombina/src/main/java/com/example/colombina/services/DocumentoService.java
@@ -1,7 +1,23 @@
 package com.example.colombina.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.colombina.model.Documento;
+import com.example.colombina.repositories.DocumentoRepository;
+
 
 @Service
 public class DocumentoService {
+
+        @Autowired
+        private DocumentoRepository documentoRepository;
+
+    public boolean verificarDocumentosCompletos(Long tramiteId) {
+        List<Documento> documentos = documentoRepository.findByTramiteId(tramiteId);
+        // Verifica si todos los documentos están aprobados
+        return documentos.stream().allMatch(Documento::isAprobado);
+    }
 }
