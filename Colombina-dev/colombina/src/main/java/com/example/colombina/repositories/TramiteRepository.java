11f6bbc745c8af1880314cc59c46f3fb1eac10ba
Carlos Mejia <mejia.carlose@javeriana.edu.co>
2024-10-23 01:13:51 -0500

    ..

--- Colombina-dev/colombina/src/main/java/com/example/colombina/repositories/TramiteRepository.java
+++ Colombina-dev/colombina/src/main/java/com/example/colombina/repositories/TramiteRepository.java
@@ -1,6 +1,5 @@
 package com.example.colombina.repositories;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +13,7 @@ import com.example.colombina.model.Tramite;
 @Repository
 public interface TramiteRepository extends JpaRepository<Tramite, Long> {
     Optional<Tramite> findByNumeroRadicado(String numeroRadicado);
-    List<Tramite> findByEstado(Tramite.EstadoTramite estado);
+    List<Tramite> findByEstado(Tramite.EstadoTramite estado);}
 
     //List<Tramite> findByFechaRadicacionAndEstado(Date fechaInicio, Date fechaFin, String tipoTramite, Tramite.EstadoTramite estado);
-}
+
